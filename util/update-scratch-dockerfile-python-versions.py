f2324ebeecb7b8b7c61538a50fffc3f97b18904f
Nikolai Korolev <korolevns98@gmail.com>
2024-05-13 08:02:46 +0100

    Add automatic updates for Dockerfile.scratch* files

---
+++ util/update-scratch-dockerfile-python-versions.py
@@ -0,0 +1,81 @@
+import argparse
+import re
+import sys
+
+import requests
+from packaging import version
+
+python_version_regex = re.compile(r"^(ARG\s+PYTHON_VERSION=).*$")
+
+
+def find_latest_python_alpine_version():
+    select_regex = re.compile(r"<select[^>]* name=\"branch\"[^>]*>([\s\S]*?)</select>")
+    session = requests.session()
+    response = session.get("https://pkgs.alpinelinux.org/packages?name=python3")
+    text = response.text
+    matches = select_regex.findall(text)
+    match len(matches):
+        case 0:
+            raise ValueError(f"Didn't find branch on the website response")
+        case 1:
+            regex_match = matches[0]
+        case _:
+            raise ValueError(f"Found two or more branches on the website response: {matches!r}")
+    option_regex = re.compile(r"<option[^>]*>([\s\S]*?)</option>")
+    matches = option_regex.findall(regex_match)
+    match len(matches):
+        case 0:
+            raise ValueError(f"Didn't find options on the website response")
+    options = [x for x in matches if x.startswith("v")]
+    latest_stable_version = options[0]
+
+    print(f"Looking at alpine version: {latest_stable_version}")
+    url = f"https://pkgs.alpinelinux.org/packages?name=python3&branch={latest_stable_version}&repo=main&arch=&maintainer="
+    print(url)
+    response = session.get(url)
+    text = response.text
+
+    version_regex = re.compile(r"<td[^>]* class=\"version\"[^>]*>([\s\S]*?)</td>")
+    matches = version_regex.findall(text)
+    match len(matches):
+        case 0:
+            raise ValueError(f"Didn't find versions on the website response")
+    versions = [version.parse(x) for x in matches]
+    release_versions = set(x.release[:2] for x in versions)
+    printable_versions = [str.join(".", map(str, x)) for x in release_versions]
+    match len(printable_versions):
+        case 0:
+            raise ValueError(f"BUG: Impossible case. Couldn't find any release version")
+        case 1:
+            printable_version = printable_versions[0]
+        case _:
+            raise ValueError(f"Multiple python versions. Can't select one: {printable_versions}")
+    return printable_version
+
+def update_python_version(filepath, version):
+    with open(filepath, 'r', encoding='utf8') as f:
+        raw_lines = f.readlines()
+    updated_lines = raw_lines
+    for i, line in enumerate(raw_lines):
+        regex_match = python_version_regex.match(line)
+        if regex_match:
+            new_line = regex_match.group(1) + version + "\n"
+            updated_lines = raw_lines[:i] + [new_line] + raw_lines[i+1:]
+            break
+    with open(filepath, 'w', encoding='utf8') as f:
+        f.writelines(updated_lines)
+
+
+
+def main(filepaths):
+    latest_python_version = find_latest_python_alpine_version()
+    for filepath in filepaths:
+        update_python_version(filepath, latest_python_version)
+    return 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Update Dockerfile scratch files ')
+    parser.add_argument('file', type=str, nargs='+', help='path to scratch files')
+    args = parser.parse_args()
+    sys.exit(main(args.file))
